4b6e5894dbbc1a514eab416b80d9b4457a10e73a
Jóhann Geir Jónsson <johanngj@siminn.is>
2019-03-22 17:38:33 +0000

    feat: Add mongodb backend

--- test/_backends.js
+++ test/_backends.js
@@ -5,11 +5,17 @@ const { defaults } = require('lodash');
 const Cache = require('../lib/cache');
 
 const backendOptions = {
-  hosts: `${process.env.MEMCACHED__HOST || '127.0.0.1'}:11211`,
+  memcached: {
+    hosts: `${process.env.MEMCACHED__HOST || '127.0.0.1'}:11211`,
+  },
+  mongodb: {
+    url: `${process.env.MONGODB_URL}`,
+    collectionName: '__testcache',
+  },
 };
 
 module.exports = function withBackends(createTestCases) {
-  ['memory', 'memcached'].forEach(backendType => {
+  ['mongodb', 'memory', 'memcached'].forEach(backendType => {
     describe(`with backend "${backendType}"`, () => {
       const cache = new Cache({
         backend: defaults({ type: backendType }, backendOptions),
